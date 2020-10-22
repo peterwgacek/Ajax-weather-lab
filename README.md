333f7ffc4ff9a4ffc0bb6bf84678bb91d81e39b8
Peter Gacek <peterwgacek@gmail.com>
2020-10-22 16:33:12 -0400

    second commit working

---
+++ README.md
@@ -0,0 +1,18 @@
+#AJAX Weather Lab
+
+
+api.openweathermap.org/data/2.5/weather?q={city name}&appid={6bfa9a264b2c6c3b661b5cd232e7bd9c}
+API: 6bfa9a264b2c6c3b661b5cd232e7bd9c
+
+
+Technologies/API used:
+    Technologies Used: HTML, CSS, JavaScript
+    API Used:
+     https://openweathermap.org/
+Features:
+
+User Stories:
+    Really helped to go about this in a group effort, though most of this was copy and paste from the class project, it really helped to get some perspective on how to approach a few things.
+Wireframes (you place hosted images in your README)
+
+Stretch Goals/Future Goals:
---
+++ js/script.js
@@ -0,0 +1,25 @@
+console.log("It's working!")
+function handleGetData(event){
+    //Stops the page from refreshing
+  event.preventDefault();
+// Load in the value of the search textbox:
+  const searchText = $("#search").val()
+  $.ajax({url: `http://api.openweathermap.org/data/2.5/weather?q=${searchText}&appid=6bfa9a264b2c6c3b661b5cd232e7bd9c`})
+    .then(
+      (data) => {
+        console.log(data);
+        $("#city").text(data.q)
+        $("#temp").text(convert(data.main.temp) + `°F`);
+        $("#feel").text(convert(data.main.feels_like) + `°F`);
+        $("#weather").text(data.weather[0].description);
+      },
+      (error) => {
+        console.log("bad request: ", error)
+      }
+    )
+}
+$('form').on("submit", handleGetData)
+
+function convert(a) {
+    return Math.round(((a-273.15)*9/5+32));
+}
